bf91bebc3e2126493e874ceaf9a80e0349364d1a
kiên lê <lexuankien122@gmail.com>
2023-10-14 23:24:40 +0700

    complete leetcode 11

--- 11.py
+++ 11.py
@@ -1,3 +1,15 @@
-print("hello word")
-print("lxk dep trai")
-print("alo alo")
+def main():
+    height = [1,8,6,2,5,4,8,3,7]
+    begin = 0
+    end = len(height) - 1
+    ans = 0
+    while (begin < end):
+        ans = max(ans, min(height[begin],height[end])*(end-begin))
+        if (height[begin] < height[end]):
+            begin = begin + 1
+        else:
+            end = end - 1
+    print(ans) 
+
+if __name__ == "__main__":
+    main()
